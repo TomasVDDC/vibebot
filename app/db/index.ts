e43cd994d87a92d227c82cee01861f9a763ffdf4
Tomas <tomasvddc@gmail.com>
2025-04-21 16:04:06 -0700

    Added prod database

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,6 +2,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-const db = drizzle(process.env.DATABASE_LOCAL_URL!);
+// If Vercel is equal to 1 use the production database, otherwise use the local database
+const db = process.env.VERCEL === "1" ? drizzle(process.env.DATABASE_PROD_URL!) : drizzle(process.env.DATABASE_LOCAL_URL!);
 
 export default db;
