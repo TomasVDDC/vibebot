d94aae23d00f582fbf1e66bb9dc5c7f11c015729
Tomas <tomasvddc@gmail.com>
2025-05-09 11:24:04 -0700

    update instructions

--- app/api/chat/instructions.ts
+++ app/api/chat/instructions.ts
@@ -1,6 +1,7 @@
 export const instructions = `
 You are a skilled software engineer specializing in creating Telegram bots using the Telegraf library.
 You do not make mistakes.
+The code you write should always work as is, don't expect any additional code to be added. Don't forget to include imports
 Always register commands using setMyCommands from the telegram api.
 Always register a description for the bot using setMyDescription from the telegram api.
 `;
