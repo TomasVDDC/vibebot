2350fd6615304dfbdd74d396242448377a3348e9
Tomas <tomasvddc@gmail.com>
2025-05-09 11:26:02 -0700

    Modified prompt to llm

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,14 +2,24 @@ import { anthropic } from "@ai-sdk/anthropic";
 import { streamObject } from "ai";
 import { answerSchema } from "./answer-schema";
 import { promptStart } from "./prompt";
+import { instructions } from "./instructions";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { prompt } = await req.json();
-  const completePrompt = promptStart + prompt;
-  console.log("This is the final prompt\n", completePrompt);
+  const { prompt, useInitialCodeTemplate } = await req.json();
+  console.log("isInitialCodeTemplate", useInitialCodeTemplate);
+
+  let completePrompt;
+  if (useInitialCodeTemplate) {
+    // In this case the prompt is only the first message of the user that we set for him to get started, so promptStart contain some generic code for a starter bot
+    completePrompt = promptStart + prompt;
+  } else {
+    // In this case the prompt should contain the previous code of the bot
+    completePrompt = instructions + prompt;
+  }
+  console.log("completePrompt", completePrompt);
 
   try {
     const result = streamObject({
@@ -23,31 +33,3 @@ export async function POST(req: Request) {
     return new Response("Error streaming object", { status: 500 });
   }
 }
-
-// import { anthropic } from "@ai-sdk/anthropic";
-// import { generateObject } from "ai";
-// import dotenv from "dotenv";
-// import { answerSchema } from "./answer-schema";
-// import { promptStart } from "./prompt";
-// dotenv.config();
-
-// export async function POST(req: Request) {
-//   console.log("Starting chat");
-//   const { prompt } = await req.json();
-//   const completePrompt = promptStart + prompt;
-
-//   const { object } = await generateObject({
-//     model: anthropic("claude-3-5-sonnet-20241022"),
-//     schema: answerSchema,
-//     prompt: completePrompt,
-//   });
-
-//   console.log(JSON.stringify(object, null, 2));
-
-//   return new Response(JSON.stringify(object), {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-// }
